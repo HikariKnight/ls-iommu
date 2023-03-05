be0ab26f10df16d300a47dd27371c35c411090bc
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2023-03-05 18:29:16 +0100

    add iommu library

---
+++ lib/iommu.go
@@ -0,0 +1,23 @@
+package iommu
+
+import (
+	"fmt"
+	"os"
+)
+
+// Function to get all IOMMU Groups on the machine
+func GetIOMMU_Groups() {
+	// Make an array of strings for us to store the IOMMU groups
+	var groups []string
+
+	files, err := os.ReadDir("/sys/kernel/iommu_groups")
+	ErrorCheck(err)
+
+	for _, group := range files {
+		if group.IsDir() {
+			groups = append(groups, group.Name())
+		}
+	}
+
+	fmt.Println(groups)
+}
